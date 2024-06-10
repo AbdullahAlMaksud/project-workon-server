358f74f9c15b53e6fec5fff4b7988bb926a5eb9a
AbdullahAlMaksud <maksud.workspace@gmail.com>
2024-06-10 23:22:46 +0600

    user post api corrected

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
 
-//middlewire
-app.use(express.json())
-app.use(cors())
+// Middleware
+app.use(express.json());
+app.use(cors());
 
-
-//mongodb
+// MongoDB
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sy9sfbb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -22,51 +21,47 @@ const client = new MongoClient(uri, {
     }
 });
 
-
 async function run() {
     try {
-
-        const usersCollection = client.db('workonDB').collection('users')
-
+        const usersCollection = client.db('workonDB').collection('users');
 
         app.get('/users', async (req, res) => {
             const result = await usersCollection.find().toArray();
-            // console.log(result)
-            res.send(result)
-        })
+            res.send(result);
+        });
+
         app.post('/users', async (req, res) => {
             const data = req.body;
-            const result = await usersCollection.insertOne(data)
-            console.log(result)
-            res.send(result)
-        })
 
+            // Check if a user with the same email already exists
+            const existingUser = await usersCollection.findOne({ email: data.email });
 
+            if (existingUser) {
+                res.status(409).send({ message: 'User already exists' });
+            } else {
+                const result = await usersCollection.insertOne(data);
+                console.log(result);
+                res.send(result);
+            }
+        });
 
-
-        // Connect the client to the server	(optional starting in v4.7)
+        // Connect the client to the server (optional starting in v4.7)
         // await client.connect();
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-
-
-
-
-
         // Ensures that the client will close when you finish/error
         // await client.close();
     }
 }
 run().catch(console.dir);
 
-
-//--------------basic test
-
+// Basic test route
 app.get('/', (req, res, next) => {
-    res.send('Workon Server is running....')
-})
+    res.send('Workon Server is running....');
+});
+
 app.listen(port, () => {
-    console.log(`Workon server is running on port ${port}`)
-})
+    console.log(`Workon server is running on port ${port}`);
+});
